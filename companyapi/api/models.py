1a9b8f32cebf3dc24846982beb0b0c88a732a5ef
gajendra <davegk59@gmil.com>
2025-03-17 18:07:53 +0530

    Create views , model

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -20,18 +20,17 @@ class Company(models.Model):
 
 #Employee Model 
 
-class Company(models.Model):
-    company_id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=50)
-    location=models.CharField(max_length=50)
-    about=models.TextField()
-    type=models.CharField(max_length=100,choices=
-            (('It','It'),
-            ('Non-IT','Non-It'),
-            ('Mobile phone','MOBILE phone')))
-    added_date = models.DateTimeField(auto_now=True)
-    active=models.BooleanField(default=True)
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+    about=models.CharField(max_length=200)
+    position=models.CharField(max_length=60,choices=(
+        ('Manager','manager'),
+        ('Software Developer','SD'),
+        ('Project Leader','pl')
+    ))
 
-    def __str__(self):
-        return self.name +'--' +self.location 
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
 
